c50a51d31a913d3d93ca4dacf77aba245e2f9312
Sam Davies <sam@razeware.com>
2015-10-14 22:51:34 +0100

    Potentially completed the playground

--- UnleashingSwift.playground/Pages/MoreAdvanced.xcplaygroundpage/Contents.swift
+++ UnleashingSwift.playground/Pages/MoreAdvanced.xcplaygroundpage/Contents.swift
@@ -3,17 +3,102 @@
 
 //: ## Optionals
 
+let names = ["Anna", "Brian", "Charlie", "Esme"]
+
+func findIndexOfName(name: String) -> Int? {
+  for (index, testName) in names.enumerate() {
+    if testName == name {
+      return index
+    }
+  }
+  return .None
+}
+
+findIndexOfName("Anna")
+findIndexOfName("Dave")
+
+let flatNumbers = ["12A", "1", "1234", "56"]
+
+if let annasIndex = findIndexOfName("Anna") {
+  flatNumbers[annasIndex]
+}
+
+func leaveMailFor(name: String) -> String {
+  guard let nameIndex = findIndexOfName(name) else { return "Please leave mail at reception" }
+  return "Leave mail for \(name) at \(flatNumbers[nameIndex])"
+}
+
+leaveMailFor("Dave")
+
 
 
 
 //: ## Enums
+//: Raw Values
+enum Activity: String {
+  case Running
+  case Swimming
+  case Cycling
+}
+
+let cycling = Activity.Cycling.rawValue
+
+//: Associated Values
+enum BillingItem {
+  case PhoneCall(minutes: Int)
+  case Data(bytes: Int)
+  case SMS(number: Int)
+}
+
+let text = BillingItem.SMS(number: 34)
+let call = BillingItem.PhoneCall(minutes: 10)
+
+
+//: Recursive
+enum ArithmeticExpression {
+  case Number(Int)
+  indirect case Addition(ArithmeticExpression, ArithmeticExpression)
+  indirect case Multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+
+let seven = ArithmeticExpression.Number(6)
+let two = ArithmeticExpression.Number(2)
+let sum = ArithmeticExpression.Addition(seven, two)
+let product = ArithmeticExpression.Multiplication(sum, ArithmeticExpression.Number(3))
 
 
 
 //: ## Pattern Matching
 
+func evaluate(expresion: ArithmeticExpression) -> Int {
+  switch expresion {
+  case .Number(let value):
+    return value
+  case .Addition(let left, let right):
+    return evaluate(left) + evaluate(right)
+  case .Multiplication(let left, let right):
+    return evaluate(left) * evaluate(right)
+  }
+}
+
+evaluate(product)
 
 
+func greeting(name: String) -> String {
+  switch name {
+  case "Dave":
+    return "Pretty sure you still don't exist"
+  case "Sam":
+    return "Hi handsome"
+  default:
+    return "Hello \(name)"
+  }
+}
+
+greeting("Dave")
+greeting("Sam")
+greeting("Esme")
 
 //: ## Protocols
 
